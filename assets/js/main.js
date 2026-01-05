33a05f4a0115c914751ced0072d7ee60f395af72
NoMercy <vrtdev907@gmail.com>
2026-01-06 02:21:00 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
-const menuToggle = document.getElementById('mobile-menu');
-const navList = document.querySelector('.nav-list');
+// يدعم جميع القوائم على كل الصفحات
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById('mobile-menu');
+    const navList = document.querySelector('.nav-list');
 
-menuToggle.addEventListener('click', () => {
-    navList.classList.toggle('active');
+    if(menuToggle && navList){
+        menuToggle.addEventListener('click', () => {
+            navList.classList.toggle('active');
+        });
+    }
 });
